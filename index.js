8c509bbd11a9596a6c68e7f34eccfc3ebdb23bb9
yves1da2vinci3 <diomadelacorano@gmail.com>
2021-11-07 09:11:10 +0000

    3e change

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
 let app = express()
-
+const PORT = process.env.PORT || 5000;
 
 
 app.get('/',(req,res)=>{
   res.send("yo it is just a test")
 })
 
-app.listen(3000,(req,res) => {
+app.listen(PORT,(req,res) => {
     console.log('running on')
 })
